b23075297314e1787fa229389c0e2c63db4b12e0
Nurfaidah636 <70933551+Nurfaidah636@users.noreply.github.com>
2021-05-03 08:27:41 +0800

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAsa-PfWYVKRzrXlV3BPONiFFL1YmlQGz8",
+    authDomain: "jobsheet11.firebaseapp.com",
+    projectId: "jobsheet11",
+    storageBucket: "jobsheet11.appspot.com",
+    messagingSenderId: "593922348671",
+    appId: "1:593922348671:web:475d8b3b42fafa6707d14a",
+    measurementId: "G-ZRL2S8027E"
+  };
+
+export default firebaseConfig;
